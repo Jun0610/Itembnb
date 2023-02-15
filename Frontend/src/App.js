f2b7f6d87769b48a20008bca1868ba4210602d30
mti <mti@purdue.edu>
2023-02-15 10:12:15 -0500

    initial setup for item post page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/homepage';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/homepage";
+import CreateItemPost from "./pages/createItemPost";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-
         <Routes>
-          <Route exact path='/' element={<Home />} />
-        </Routes>
-      </Router>
+          <Route exact path="/" element={<Home />} />{" "}
+          <Route exact path="/create-item-post" element={<CreateItemPost />} />{" "}
+        </Routes>{" "}
+      </Router>{" "}
     </div>
   );
 }
---
+++ Frontend/src/pages/createItemPost.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const createItemPost = () => {
+  return (
+    <div>
+      <h1> This is create an item post </h1>{" "}
+    </div>
+  );
+};
+
+export default createItemPost;
