51dad6c0d5c7e0512163cdb7227ead6215dd660f
afshirazi <ashirazi@purdue.edu>
2023-02-16 23:03:45 -0500

    Added conditional rendering to posts

--- Frontend/src/components/post.js
+++ Frontend/src/components/post.js
@@ -8,18 +8,28 @@ const Post = ({item}) => {
     const rating = 4;
 
     return ( 
-        <div className="card">
-        <img src={image} className="card-img-top custom-card-img" alt="..." />
+        
+      <div className="card">
+
+        {image != "" && 
+          <img src={image} className="card-img-top custom-card-img" alt="..." />}
+        {image === "" &&
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg" className="card-img-top custom-card-img" alt="..." />}
+        
         <div className="row">
-          <div className="card-body custom-card-body">
+
+          <div className="card-body custom-card-body-left">
             <h5 className="card-title"> {title} </h5>
             <p className="card-text item-desc"> {description} </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn custom-card-button">Go somewhere</a>
           </div>
-          <div className="card-body custom-card-body">
-            <p className="card-text"> {price} </p>
-            <p className="card-text"> {rating}/5 </p>
-          </div>
+
+        {isRequest === false &&
+          <div className="card-body custom-card-body-right">
+            <p className="card-text item-pr"> ${price} </p>
+            <p className="card-text item-pr"> {rating}/5 </p>
+          </div>}
+
         </div>
       </div>
     );
