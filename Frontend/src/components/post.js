b2caaab314866f7e2fec3edd569c33926a0665cc
afshirazi <ashirazi@purdue.edu>
2023-02-16 19:07:34 -0500

    Created post component "cards" and relevant CSS

--- Frontend/src/components/post.js
+++ Frontend/src/components/post.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/post.css";
 
 const Post = ({item}) => {
     const {title, description, price, image, isRequest} = item;
 
     return ( 
-        <div className="card" >
-        <img src="..." className="card-img-top" alt="..." />
+        <div className="card">
+        <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title"> {title} </h5>
           <p className="card-text"> {description} </p>
--- Frontend/src/pages/homepage.js
+++ Frontend/src/pages/homepage.js
@@ -1,18 +1,100 @@
 import React from "react";
 import Post from "../components/post";
 
+import "../styles/homepage.css";
+
+
 const Homepage = () => {
   return (
     <div>
       <h1>Item posts</h1>
-        <Post item={
-            {title: "Item 1",
-            description: "This is item 1",
-            price: 10,
-            image: "",
-            isRequest: false}
-        } />
+        <div className="cardcontainer">
+          <Post item={
+                {title: "Item 1",
+                description: "This is item 1",
+                price: 10,
+                image: "",
+                isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } /><Post item={
+                {title: "Item 1",
+                description: "This is item 1",
+                price: 10,
+                image: "",
+                isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+        </div>
       <h1>Item requests</h1>
+      <div className="cardcontainer">
+          <Post item={
+                {title: "Item 1",
+                description: "This is item 1",
+                price: 10,
+                image: "",
+                isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } /><Post item={
+                {title: "Item 1",
+                description: "This is item 1",
+                price: 10,
+                image: "",
+                isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+              <Post item={
+                  {title: "Item 1",
+                  description: "This is item 1",
+                  price: 10,
+                  image: "",
+                  isRequest: false}
+              } />
+        </div>
     </div>
   );
 };
