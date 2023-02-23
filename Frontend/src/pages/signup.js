a7f5b4c1b689bd997ab5a980825223388b6b2521
afshirazi <ashirazi@purdue.edu>
2023-02-22 21:46:23 -0500

    fixed look of the signup page and added verification

--- Frontend/src/pages/signup.js
+++ Frontend/src/pages/signup.js
@@ -8,13 +8,18 @@ const ImgUpload =({
   })=>
     <label htmlFor="photo-upload" className="custom-file-upload fas">
       <div className="img-wrap" >
-        <img for="photo-upload" src={src} alt="upload image here" id="textfix"/>
+        <img for="photo-upload" src={src} alt="upload image here"/>
       </div>
       <input id="photo-upload" type="file" onChange={onChange}/> 
     </label>
     
 const SignUp = () => {
     const [selectedImage, setSelectedImage] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [confirmPass, setConfirmPass] = useState(true);
 
     const handleImageChange = (e) => {
         if (e.target.files && e.target.files[0]) {
@@ -23,28 +28,64 @@ const SignUp = () => {
         }
     };
 
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleConfirmPassword = (e) => {
+        if (password === e.target.value) {
+            setConfirmPass(true);
+        } else {
+            setConfirmPass(false);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (confirmPass)
+            console.log("submit");
+        else    
+            console.log("error");
+    };
+
     return ( 
-        <div id="outer-container">
-            <div className="container" id="inner-container">
-                <h1>Sign Up</h1>
+        <div className="fullscreen">
+            <div id="inner-container">
+                <h1 id="signup-header">Sign Up</h1>
                 <div className="row">
-                    <div className="col">        
-                        <form>
-                            <label className="form-label" htmlFor="name">Enter your name</label>
-                            <input className="form-control" type="text" name="username" id="username" />
-                            <label className="form-label" htmlFor="email">Enter email</label>
-                            <input className="form-control" type="email" name="email" id="email" />
-                            <label className="form-label" htmlFor="password">Enter a password</label>
-                            <input className="form-control" type="password" name="password" id="password" />
-                            <label className="form-label" htmlFor="password2">Confirm password</label>
-                            <input className="form-control" type="password" name="password2" id="password2" />
+                    <div className="col">
+                        <form className="custom-col">
+                            <div id="center-img">
+                                <ImgUpload onChange={handleImageChange} src={selectedImage}/>
+                            </div>
+                            <label className="form-label" htmlFor="text">Add user description</label>
+                            <textarea className="form-control" name="description" id="description" cols="30" rows="10" onChange={handleDescriptionChange}></textarea>
                         </form>
                     </div>
-                    <div className="col custom-col">
-                        <form>
-                            <ImgUpload onChange={handleImageChange} src={selectedImage}/>
-                            <label className="form-label" htmlFor="text">Add user description</label>
-                            <textarea className="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+                    <div className="col">        
+                        <form id="custom-form" onSubmit={handleSubmit} novalidate>
+                            <label className="form-label" htmlFor="name">Enter your name</label>
+                            <input className="form-control" type="text" name="name" id="name" onChange={handleNameChange} required/>
+                            <label className="form-label" htmlFor="email">Enter email</label>
+                            <input className="form-control" type="email" name="email" id="email" onChange={handleEmailChange} required/>
+                            <label className="form-label" htmlFor="password">Enter a password</label>
+                            <input className="form-control" type="password" name="password" id="password" onChange={handlePasswordChange} required/>
+                            <label className="form-label" htmlFor="password2">Confirm password</label>
+                            <input className="form-control" type="password" name="password2" id="password2" onChange={handleConfirmPassword} required/>
+                            {!confirmPass && <span id="password-error" className="error-message">Your passwords don't match!</span>}
+                            <button className="btn btn-primary custom-btn">Sign Up</button>
                         </form>
                     </div>
                 </div>
