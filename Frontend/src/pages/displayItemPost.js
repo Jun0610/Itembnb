b7a9e6d15c19fa2f76f4723f04470c5f8329d45f
mti <mti@purdue.edu>
2023-02-25 14:50:14 -0500

    coomit

--- Frontend/src/pages/displayItemPost.js
+++ Frontend/src/pages/displayItemPost.js
@@ -22,6 +22,8 @@ const DisplayItemPost = () => {
   const [categories, setCategories] = React.useState([]);
   const maxNumber = 20;
 
+  const authUser = useContext(userContext);
+
   useEffect(() => {
     // call API to fetch the item data
     async function fetchCategories() {
