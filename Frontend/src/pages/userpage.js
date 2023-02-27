935b61f5e69db46b6841385ce16124f52ace9dd8
Sara Xiao <xiao286@purdue.edu>
2023-02-26 20:42:58 -0500

    Improve userpage item functionality - Still not perfect, but better than before

--- Frontend/src/pages/userpage.js
+++ Frontend/src/pages/userpage.js
@@ -31,44 +31,95 @@ const Userpage = () => {
 
   // Get user info from the server
   const [userInfo, setUserInfo] = React.useState([]);
+  const [userItems, setUserItems] = React.useState([]);
+
+  const getOneItem = async() => {
+    // const itemData = await ItemService.getItem(userInfo.postedItems[i]);
+    const itemData = await ItemService.getItem("63ec624c09acb0b1c1fe9173");
+    console.log("ITEM DATA RECIVED", itemData);
+    setUserItems(() => [
+        userItems,
+        <Post item={
+            {name: itemData.name,
+            description: itemData.description,
+            price: itemData.price,
+            image: itemData.image,
+            isRequest: false}
+        } />
+    ]);
+    console.log("add item", userItems);
+  };
+
   useEffect(() => {
     async function fetchData() {
         const data = await UserService.getUser(id);
         setUserInfo(data);
+
+        const returnUserItems = async (data) => {
+            console.log("data", data);
+
+            if (data.postedItems.length == 0) {
+                setUserItems(<div>{data.name} has no items!</div>);
+            }
+            else {
+                for (let i = 0; i < data.postedItems.length; i++) {
+                    console.log("add item??");
+                    await getOneItem();
+                }
+            }
+        }
+        // returnUserItems(data);
+
     } 
     fetchData();
+
+    /*
+    async function loadViews() {
+        const componentPromises =
+        userInfo().map(async subreddit => {
+            const View = await importView(subreddit);
+            return <View key={shortid.generate()} />;
+        });
+
+        Promise.all(componentPromises).then(setViews);
+    }
+    loadViews();
+    */
+
   }, []);
 
   if (! Object.keys(userInfo).length) { // if user info hasn't loaded
     return ""; // just show a blank screen
   }
 
-  const returnUserItems = async () => {
+  const returnUserItems = () => {
     if (userInfo.postedItems.length == 0) {
         return <div>{userInfo.name} has no items!</div>
     }
 
     let userItems = [];
     for (let i = 0; i < userInfo.postedItems.length; i++) {
-        async function fetchData() {
-            const itemData = await ItemService.getItem(userInfo.postedItems[i]);
-            console.log("ITEM DATA RECIVED", itemData);
-            /*
-            userItems.push(
-                <Post key={i} item={
-                    {name: itemData.name,
-                    description: itemData.description,
-                    price: itemData.price,
-                    image: itemData.image,
-                    isRequest: false}
-                } />
-            );
-            */
-        }
-        fetchData();
+        let itemData = ItemService.getItem(userInfo.postedItems[i]);
+        userItems.push(
+            <Post item={
+                {name: itemData.name,
+                description: itemData.description,
+                price: itemData.price,
+                image: itemData.image,
+                isRequest: false}
+            } />
+        );
     }
     return userItems;
   }
+
+  /*
+  const wrapper = () => {
+    if (returnUserItems is Promise) {
+        return "";
+    }
+    return returnUserItems();
+  } */
 
   return (
     <div id="page_content_container">
