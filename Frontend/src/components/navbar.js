8bf5454ca5d0f5311829d8c906d35fa22e534692
Jun0610 <lim321@purdue.edu>
2023-03-02 12:43:43 -0500

    fixed issue where profile pic was not loading on first entry

--- Frontend/src/components/navbar.js
+++ Frontend/src/components/navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
       }
     }
     fetchUserData();
-  }, [])
+  }, [sessionStorage.getItem('curUser')])
 
   const logout = (e) => {
     e.preventDefault();
@@ -64,7 +64,7 @@ const Navbar = () => {
             </NavLink>
           </li>}
           {authUser.isAuth && <li>
-            <NavLink to={"/user/"+authUser.user.user._id} className="nav-link-img">
+            <NavLink to={"/user/" + authUser.user.user._id} className="nav-link-img">
               <img src={userData.profilePic} alt="" className='nav-img' />
             </NavLink>
           </li>}
