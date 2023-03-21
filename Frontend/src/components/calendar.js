53e475c28591a251117b50cde51d3ace1d8f34c4
afshirazi <ashirazi@purdue.edu>
2023-03-20 21:47:19 -0400

    Stylized calendar some more, added unavailability

--- Frontend/src/components/calendar.js
+++ Frontend/src/components/calendar.js
@@ -3,15 +3,29 @@ import React from 'react';
 
 import '../styles/calendar.css';
 
+const itemUnavail = ({date, view}) => {
+    if (view === 'month') {
+        if (date.getTime() < Date.now()) 
+            return true;
+    }
+        
+}
+
 const ItemCalendar = () => {
     const [date, setDate] = React.useState(new Date());
     
+    const onChangePrint = (date) => {
+        console.log("Date selected: ");
+        console.log(date);
+        setDate(date);
+    }
+
     return (
         <div>
         <Calendar
-            onChange={setDate}
-            value={date}
+            onChange={onChangePrint}
             selectRange={true}
+            tileDisabled={itemUnavail}
         />
         </div>
     );
