ec8c84ede92026f192b0599f50601a7a034b9d71
mti <mti@purdue.edu>
2023-03-26 00:13:16 -0400

    fix bugs

--- Frontend/src/pages/emailStatusPage.js
+++ Frontend/src/pages/emailStatusPage.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect} from 'react';
 import userContext from "../contexts/userContext";
 import {useNavigate, useParams} from 'react-router-dom';
 import UserService from '../tools/userService';
+import SocketService, {socket} from '../tools/socketService';
 
 const EmailStatusPage = () => {
   const { id } = useParams();
@@ -13,6 +14,8 @@ const EmailStatusPage = () => {
         const data = await UserService.getUserData(id);
         authUser.login(data.data);
         sessionStorage.setItem('curUser', JSON.stringify(data.data)); 
+        SocketService.connect();
+        socket.emit('sendId', data.data.email);
     }
     // need to login the user and set to the authUser thing
     // need to redirect user
