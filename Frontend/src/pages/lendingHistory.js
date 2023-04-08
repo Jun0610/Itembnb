4c2391797a54741bffb4c3aa22f3088b0721a8c8
mti <mti@purdue.edu>
2023-04-08 11:43:35 -0400

    lending history minimize/maximize

--- Frontend/src/pages/lendingHistory.js
+++ Frontend/src/pages/lendingHistory.js
@@ -8,6 +8,7 @@ const LendingHistory = () => {
     const [lendingHist, setLendingHist] = useState([]);
     const authUser = useContext(userContext);
     const [loading, setLoading] = useState(true);
+    const [minimize, setMinimize] = useState(false);
     const nav = useNavigate();
 
     useEffect(() => {
@@ -24,15 +25,29 @@ const LendingHistory = () => {
     return (
         <div>  
             <div>LendingHistory</div>
+            <div onClick={() => setMinimize(!minimize)}>Minimize Mode</div>
             {lendingHist === null || lendingHist === undefined || lendingHist.length === 0 ? <div>You don't have any lending history yet!</div>
             : <div>
-            {lendingHist.map((e, i) => (<div key={i}>
+            {lendingHist.map((e, i) => (
+            minimize ? 
+            <div key={i} className='m-3 p-3 border-b-2 border-b-yellow-600 flex gap-4 justify-between'>
+                <div>
+                    {e.item.name}
+                </div> 
+                <div>
+                    {e.borrower.name}
+                </div> 
+                <div>
+                    from {new Date(e.reservation.startDate).toDateString()} to {new Date(e.reservation.endDate).toDateString()}
+                </div>    
+            </div> :
+            <div key={i} className='m-3 p-3 border-2 border-yellow-400 rounded-3xl'>
                 <div className='grid grid-cols-5'>
                     <div>{e.item.name}</div>
-                    <div>{e.reservation.startDate}</div>
-                    <div>{e.reservation.endDate}</div>
+                    <div>{new Date(e.reservation.startDate).toDateString()}</div>
+                    <div>{new Date(e.reservation.endDate).toDateString()}</div>
                     <div>{e.borrower.name}</div>
-                    <img src={e.borrower.profilePic} alt="" className="owner-img" onClick={() => nav(`/user-profile/${e.borrower._id}`)} />
+                    <img src={e.borrower.profilePic} alt="" style={{cursor: "pointer"}} className="owner-img" onClick={() => nav(`/user/${e.borrower._id}`)} />
                 </div>
                 </div>))}
             </div>}
