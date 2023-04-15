1dd8e111910455c27167f6c3af797de855ef3d15
mti <mti@purdue.edu>
2023-04-15 07:53:41 -0400

    sorting filter for lending history; borrower component for lending history; change UI

--- Frontend/src/pages/borrowingHistory.js
+++ Frontend/src/pages/borrowingHistory.js
@@ -21,7 +21,7 @@ const BorrowingHistory = () => {
         getAllBorrowingResv().then(() => setLoading(false));
     }, []);
 
-    const orderByDate = (order) => {
+    const changeOrder = (order) => {
         var newBH = [];
         if (order === 'date') {   
             setIsAscN(true)
@@ -63,39 +63,70 @@ const BorrowingHistory = () => {
                     <div class="w-11 h-6 bg-blue-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[8px] after:left-[2px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-red-600"></div>
                     <span class="ml-3 text-sm font-medium">Minimize Mode</span>
                 </label>
-                <div onClick={() => orderByDate('date')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>{isAscD ? 'List By Most Recent' : 'List by Least Recent'}</div>
-                <div onClick={() => orderByDate('name')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>List By Alphabetical Order &nbsp; {isAscN ? '(A-Z)' : '(Z-A)'}</div>
+                <div onClick={() => changeOrder('date')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>{isAscD ? 'List By Most Recent' : 'List by Least Recent'}</div>
+                <div onClick={() => changeOrder('name')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>List By Alphabetical Order &nbsp; {isAscN ? '(A-Z)' : '(Z-A)'}</div>
             </div>
             {borrowingHist && borrowingHist.length !== 0 ? 
             <div>
-                {borrowingHist.map((_, i) => (
+                {borrowingHist.map((e, i) => (
                 minimize ? 
                 <div key={i} className='m-3 p-3 border-b-2 border-b-yellow-600 flex gap-4 justify-between'>
-                    <div style={{cursor: "pointer"}} onClick={() => {nav(`/selected-item-post/${borrowingHist[i].item._id}`)}}>
-                        {borrowingHist[i].item.name}
-                    </div>  
+                    <div style={{cursor: "pointer"}} onClick={() => {nav(`/selected-item-post/${e.item._id}`)}}>
+                        {e.item.name}
+                    </div> 
+                    <div style={{cursor: "pointer"}} onClick={() => {nav(`/user/${e.lender._id}`)}}>
+                        {e.lender.name}
+                    </div>   
                     <div>
-                        from {new Date(borrowingHist[i].reservation.startDate).toDateString()} to {new Date(borrowingHist[i].reservation.endDate).toDateString()}
+                        From {new Date(e.reservation.startDate).toDateString()} to {new Date(e.reservation.endDate).toDateString()}
                     </div>    
                 </div> :
-                <div key={i} className='m-3 p-3 border-4 rounded-3xl border-yellow-400'>
+                <div key={i} className='m-3 p-3 border-2 rounded-3xl border-yellow-400'>
                     <div className='grid grid-cols-3'>
                         <div>
-                            <img onClick={() => {nav(`/selected-item-post/${borrowingHist[i].item._id}`)}} src={borrowingHist[i].item.images[0]} style={{cursor: "pointer"}} alt="pretty" className='h-32 object-scale-down'/>
-                            <div>{borrowingHist[i].item.name}</div>
-                        </div>
-                        <div>.
-                            <img onClick={() => {nav(`/user/${borrowingHist[i].lender._id}`)}} src={borrowingHist[i].lender.profilePic} style={{cursor: "pointer"}} alt="pretty" className='h-32 object-scale-down'/>
-                            <div>{borrowingHist[i].lender.name}</div>
+                            <img onClick={() => {nav(`/selected-item-post/${e.item._id}`)}} src={e.item.images[0]} style={{cursor: "pointer"}} alt="pretty" className='h-24 object-scale-down'/>
+                            <div class="flex gap-0 m-1">
+                                <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                    Item Name
+                                </div>
+                                <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                    {e.item.name}
+                                </div>
+                            </div>
                         </div>
                         <div>
-                            <div>From: {new Date(borrowingHist[i].reservation.startDate).toDateString()}</div>
-                            <div>To: {new Date(borrowingHist[i].reservation.endDate).toDateString()}</div>
+                            <img onClick={() => {nav(`/user/${e.lender._id}`)}} src={e.lender.profilePic} style={{cursor: "pointer"}} alt="pretty" className='h-24 object-scale-down'/>
+                            <div class="flex gap-0 m-1">
+                                <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                    Lender Name
+                                </div>
+                                <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                    {e.lender.name}
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="flex gap-0 m-1">
+                                <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                    From
+                                </div>
+                                <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                    {new Date(e.reservation.startDate).toDateString()}
+                                </div>
+                            </div>
+                            <div class="flex gap-0 mt-2 m-1">
+                                <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                    To
+                                </div>
+                                <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                    {new Date(e.reservation.endDate).toDateString()}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>))}
             </div> : 
-            <div>You have no borrowing history yet!</div>}
+            <div className='text-xl font-bold m-3'>You have no borrowing history yet!</div>}
         </div>
     )
   } else return <Loading />
--- Frontend/src/pages/lendingHistory.js
+++ Frontend/src/pages/lendingHistory.js
@@ -7,9 +7,13 @@ import { useNavigate } from 'react-router-dom';
 
 const LendingHistory = () => {
     const [lendingHist, setLendingHist] = useState([]);
+    const [borrowers, setBorrowers] = useState([]);
+    const [OGLendingHist, setOGLendingHist] = useState([])
     const authUser = useContext(userContext);
     const [loading, setLoading] = useState(true);
     const [minimize, setMinimize] = useState(false);
+    const [isAscD, setIsAscD] = useState(true);
+    const [isAscN, setIsAscN] = useState(true);
     const nav = useNavigate();
 
     useEffect(() => {
@@ -17,11 +21,19 @@ const LendingHistory = () => {
             // use sessionUser instead of authUser since authUser + userEffect is unreliable
             const sessionUser = JSON.parse(sessionStorage.getItem('curUser'));
             const data = await ReservationService.getAllPastLendingHistory(sessionUser._id);
-            setLendingHist(data.data);
+            console.log(data.data)
+            setLendingHist(data.data)
+            setOGLendingHist(data.data)
             /*
             SocketService.connect();
             socket.emit('sendId', sessionUser.email);
             */
+           const newB = []
+           if (data.data)
+                data.data.forEach(e => {
+                    if (!newB.includes(e.borrower.name)) newB.push(e.borrower.name)
+                })
+           setBorrowers(newB)
         }
         getAllLendingResv().then(() => setLoading(false));
     }, []);
@@ -30,39 +42,118 @@ const LendingHistory = () => {
         return nav("/login-required");
     }
 
+    const changeOrder = (order) => {
+        var newBH = [];
+        if (order === 'date') {   
+            setIsAscN(true)
+            if (isAscD) 
+                newBH = lendingHist.sort(function (a, b) {
+                    return new Date(a.reservation.startDate) - new Date(b.reservation.endDate)
+                })
+            else
+                newBH = lendingHist.sort(function (a, b) {
+                    return new Date(b.reservation.startDate) - new Date(a.reservation.endDate)
+                })
+            setIsAscD(!isAscD)
+        } else {
+            setIsAscD(true)
+            if (isAscN)
+                newBH = lendingHist.sort(function(a, b ) {
+                    var x = a.borrower.name.localeCompare(b.borrower.name)
+                    if (x === 0) return new Date(a.reservation.startDate) - new Date(b.reservation.endDate)
+                    else return x
+                })
+            else 
+                newBH = lendingHist.sort(function(a, b ) {
+                    var x = b.borrower.name.localeCompare(a.borrower.name)
+                    if (x === 0) return new Date(a.reservation.startDate) - new Date(b.reservation.endDate)
+                    else return x
+                })
+            setIsAscN(!isAscN)
+        }
+        setLendingHist([...newBH])
+    }
+
+    const onReset = () => {
+        setLendingHist(OGLendingHist)
+    }
+
+    const onSelectBorrower = (name) => {
+        setLendingHist(OGLendingHist.filter(e => e.borrower.name === name))
+    }
+
   if (!loading) {
     return (
         <div>  
             <div className='m-3 flex gap-4'>
-                <div className='text-xl font-bold text-yellow-400'>Borrowing History</div>
+                <div className='text-xl font-bold text-yellow-400'>Lending History</div>
                 <label class="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" value="" class="sr-only peer" onClick={() => setMinimize(!minimize)}/>
-                    <div class="w-11 h-6 bg-blue-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-red-600"></div>
+                    <div class="w-11 h-6 bg-blue-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[8px] after:left-[2px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-red-600"></div>
                     <span class="ml-3 text-sm font-medium">Minimize Mode</span>
                 </label>
+                <div onClick={() => changeOrder('date')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>{isAscD ? 'List By Most Recent' : 'List by Least Recent'}</div>
+                <div onClick={() => changeOrder('name')} className='items-center font-medium text-sm p-2 bg-yellow-400 rounded-lg text-white' style={{cursor: "pointer"}}>List By Alphabetical Order &nbsp; {isAscN ? '(A-Z)' : '(Z-A)'}</div>
             </div>
-            {lendingHist === null || lendingHist === undefined || lendingHist.length === 0 ? <div>You don't have any lending history yet!</div>
+            {lendingHist === null || lendingHist === undefined || lendingHist.length === 0 ? <div className='text-xl font-bold m-3'>You don't have any lending history yet!</div>
             : <div>
+                <div className="flex gap-3">
+                    <div className='items-center p-2 text-medium font-bold text-yellow-400'>View by borrower: </div>
+                    {borrowers.map((e, i) => (<div key={i} onClick={() => onSelectBorrower(e)} className='items-center font-medium text-sm p-2 bg-yellow-100 rounded-lg' style={{cursor: "pointer"}}>{e}</div>))}
+                    <div onClick={() => onReset()} className='items-center font-medium text-sm p-2 bg-yellow-100 rounded-lg' style={{cursor: "pointer"}}>Reset</div>
+                </div>
             {lendingHist.map((e, i) => (
             minimize ? 
             <div key={i} className='m-3 p-3 border-b-2 border-b-yellow-600 flex gap-4 justify-between'>
-                <div>
-                    {e.item.name}
-                </div> 
                 <div style={{cursor: "pointer"}} onClick={() => nav(`/user/${e.borrower._id}`)}>
                     {e.borrower.name}
                 </div> 
                 <div>
-                    from {new Date(e.reservation.startDate).toDateString()} to {new Date(e.reservation.endDate).toDateString()}
+                    {e.item.name}
+                </div> 
+                <div>
+                    From {new Date(e.reservation.startDate).toDateString()} to {new Date(e.reservation.endDate).toDateString()}
                 </div>    
             </div> :
             <div key={i} className='m-3 p-3 border-2 border-yellow-400 rounded-3xl'>
-                <div className='grid grid-cols-5'>
-                    <div>{e.item.name}</div>
-                    <div>{new Date(e.reservation.startDate).toDateString()}</div>
-                    <div>{new Date(e.reservation.endDate).toDateString()}</div>
-                    <div>{e.borrower.name}</div>
-                    <img src={e.borrower.profilePic} alt="" style={{cursor: "pointer"}} className="owner-img" onClick={() => nav(`/user/${e.borrower._id}`)} />
+                <div className='grid grid-cols-3'>
+                    <img src={e.borrower.profilePic} alt="" style={{cursor: "pointer"}} className="owner-img h-32" onClick={() => nav(`/user/${e.borrower._id}`)} />
+                    <div>
+                        <div class="flex gap-0 m-1">
+                            <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                Name
+                            </div>
+                            <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                {e.borrower.name}
+                            </div>
+                        </div>
+                        <div class="flex gap-0 m-1">
+                            <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                Borrowed
+                            </div>
+                            <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                {e.item.name}
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="flex gap-0 m-1">
+                            <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                From
+                            </div>
+                            <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                {new Date(e.reservation.startDate).toDateString()}
+                            </div>
+                        </div>
+                        <div class="flex gap-0 m-1">
+                            <div class="rounded-l-lg content-center p-1 font-semibold text-white" style={{ backgroundColor: "#F7D65A" }}>
+                                To
+                            </div>
+                            <div class="bg-white/25 rounded-r-lg content-center p-1">
+                                {new Date(e.reservation.endDate).toDateString()}
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 </div>))}
             </div>}
